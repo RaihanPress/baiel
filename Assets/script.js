b673441d0a12fa79d9a3e7d5832c672262243ba7
RaihanPress <70082282+RaihanPress@users.noreply.github.com>
2021-09-05 09:27:46 +0530

    Add files via upload

---
+++ Assets/script.js
@@ -0,0 +1,27 @@
+(() => {
+    $ = el => document.querySelector(el)
+    $$ = el => document.querySelectorAll(el)
+    window.addEventListener('scroll', function () {
+        $('nav').classList.toggle('sticky', window.scrollY > 50)
+    })
+    $$('img').forEach(i => i.alt = 'Image Unavailable at ' + i.src)
+    const date = new Date()
+    if(date.getUTCFullYear() >= 2021 && date.getMonth() >= 8 && date.getDate() > 5){
+        $('#offer').remove()
+    }
+    window.addEventListener('hashchange',function(e){
+        if(this.location.hash !== "" && $(this.location.hash)){
+            this.setTimeout(()=>{
+                window.scrollTo(0,$(this.location.hash).offsetTop-60)
+            },10)
+        }
+    })
+    $$('.nav a').forEach(a=>{
+        a.addEventListener('click',function(e){
+            e.preventDefault()
+            $$('.nav a').forEach(a=>a.classList.remove('active'))
+            e.target.classList.add('active')
+            window.scrollTo(0,$('#'+e.target.href.split('#')[1]).offsetTop-60)
+        })
+    })
+})()
